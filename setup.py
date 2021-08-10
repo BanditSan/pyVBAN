bcd8c454f9dfadb28b5719fc1f54f14cf24df32b
BanditSan <marius.zukaitis@gmail.com>
2021-08-10 09:08:06 +0300

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup
 setup(
     name='pyvban',
     version='1.0.0',
+    url='https://github.com/BanditSan/pyVBAN',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     install_requires=[
         'pyaudio',
     ],
+    packages=['pyVBAN.py'],
 )
